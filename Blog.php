a3022329603a31633a4cf9986960fcfac7d1178b
MrShadowgrin <lshartley2@gmail.com>
2015-03-09 23:25:10 -0700

    Bitches aint shit 2

--- Blog.php
+++ Blog.php
@@ -1,9 +1,59 @@
 <?php
+date_default_timezone_set("America/Los_Angeles");
+
 class Blog {
 	public $title;
 	public $description;
+    
+    private $content = [];
+    
 	public function __construct($title, $description){
 		$this->title = $title;
 		$this->description = $description;
 	}
+    
+    public function addContent($content)
+    {
+        $this->content[] = (object) [
+            'title' => $content['title'],
+            'dateline' => $this->parseDate($content['dateline']),
+            'content' => $this->parseContent($content['content'])
+        ];
+    }
+    
+    public function parseContent($post)
+    {
+        // http://php.net/manual/en/function.preg-replace.php
+        $badWords = [
+            '/fuck/', '/bitch/',
+            '/whore/', '/ass/',
+            '/tits/', '/nigger/',
+            '/fucker/', '/vagina/',
+            '/penis/', '/nigga/'  
+        ];
+        return preg_filter($badWords, '*****', $post);    
+    }
+    
+    public function parseDate($timestamp)
+    {
+        // http://php.net/manual/en/function.date.php
+        return date("F j, Y, g:i a", $timestamp); 
+    }
+    
+    public function getAll()
+    {
+        return (object) $this->content;    
+    }
 }
+/*
+Posts{
+	Content{
+		Fonts
+		Color
+		Censoring
+		Images
+	}
+	
+
+}
+*/
--- index.php
+++ index.php
@@ -1,13 +1,38 @@
 <?php
 require_once("Blog.php");
 $blog = new Blog("Jung", "He's such a Junghole");
+
+$blog->addContent([
+    'title' => 'asdfa',
+    'dateline' => time(),
+    'content' => 'fuck'
+]);
 ?>
 <html>
 <head>
 	<title></title>
 </head>
 <body>
-<h1><?=$blog->title?></h1>
-<div><?=$blog->description?></div>
+<h1>
+	<?=$blog->title?><br>
+	<small><?=$blog->description?></small>
+</h1>
+
+<div class="blog">
+    <?php foreach($blog->getAll() as $item): ?>
+	<div class="item">
+		<h3 class="title"><?=$item->title?></h3>
+		<div class="dateline"><?=$item->dateline?></div>
+		<div class="content"><?=$item->content?></div>
+		<div class="comments">
+			<div class="comment-item">
+				<div class="author"></div>
+				<div class="dateline"></div>
+				<div class="comment-content"></div>
+			</div>
+		</div>
+	</div>
+    <?php endforeach; ?>
+</div>
 </body>
 </html>
